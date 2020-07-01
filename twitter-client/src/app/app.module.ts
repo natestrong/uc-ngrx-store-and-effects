ef8bb291a02b8f9c0a92a018dedb745644a037ea
Nathan Strong <nathanstrong.fx@gmail.com>
2020-06-30 21:09:18 -0700

    cool

--- twitter-client/src/app/app.module.ts
+++ twitter-client/src/app/app.module.ts
@@ -1,8 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MetaReducer, StoreModule} from '@ngrx/store';
+import {storeFreeze} from 'ngrx-store-freeze';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import {environment} from '../environments/environment';
+
+export const metaReducers: MetaReducer<any>[] = !environment.production
+  ? [storeFreeze]
+  : [];
 
 @NgModule({
   declarations: [
@@ -10,9 +20,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}, {metaReducers}),
+    EffectsModule.forRoot([]),
+    environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
